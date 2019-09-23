9a41578cbc140ad56864e5c744851fef994f753a
AMAN SHARMA <amansharmaammu2@gmail.com>
2019-09-23 11:38:15 +0530

    Single url Recipe are deleted

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-// import Router from "./components/Router"
+// import App from "./components/App"
 import * as serviceWorker from './serviceWorker';
-import Router from './components/Router';
+import App from './App';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
